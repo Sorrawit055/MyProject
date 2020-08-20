743e81269fc5924f9565c628fe3f1f40f502aea8
Nattanai <614259013@webmail.npru.ac.th>
2020-08-20 12:08:18 +0700

    edfit testing

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("NPRU1");
+		System.out.println("GayGolf");
 		System.out.println("NPRU2");
 		System.out.println("NPRU3");
 	}
