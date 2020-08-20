1839350ad03a9dc7cbccee49aca02bdeffa7d140
Nattanai <614259013@webmail.npru.ac.th>
2020-08-20 11:32:44 +0700

    Update text  to Software engieering

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("NPRU");
-		System.out.println("NPRU");
-		System.out.println("NPRU");
+		System.out.println("NPRU1");
+		System.out.println("NPRU2");
+		System.out.println("NPRU3");
 	}
 }
